a6285bbf860a8b02a80a5fa616051be726f927d4
rangga.alrasya <rangga.alrasya@gmail.com>
2023-01-10 18:37:56 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('navbar')
+
+@section('container')
+    <div class="container">
+        <div class="row">
+            <div class="col-10">
+                <h1 class="mt-3">Hello, world!</h1>
+            </div>
+        </div>
+    </div>
+@endsection
