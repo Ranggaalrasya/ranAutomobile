204b4de314767f5c44d66f945c7ff2b4f7b40889
Vincencio Gabriel Frederic Sanjaya <vincenciogfs15@gmail.com>
2023-01-11 11:55:27 +0700

    Feat: [VG] Adding footer layouts

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,35 @@
+<div class="container-footer">
+    <div class="footer-atas">
+        <div class="info">
+            <img src="/assets/img/whte_logo.png" alt="RanAutoMobile" class="img-footer" height="108px">
+            <p class="adress-footer">Jalan Sukun Raya No.09, Besito Kulon, Besito, Kec. Gebog, Kabupaten Kudus, Jawa
+                Tengah 59333
+            </p>
+        </div>
+        <div class="info-2">
+            <div class="company">
+                <ul class="list-footer">
+                    <li class="list-item-footer"><a href="#" class="link-footer">AKUN SAYA</a></li>
+                    <li class="list-item-footer"><a href="#" class="link-footer">CABANG</a></li>
+                    <li class="list-item-footer"><a href="#" class="link-footer">KEBIJAKAN PRIVASI</a></li>
+                    <li class="list-item-footer"><a href="#" class="link-footer">SYARAT & KETENTUAN</a></li>
+                </ul>
+            </div>
+            <div class="contact">
+                <form class="d-flex" role="search">
+                    <input class="form-contact" type="search" placeholder="Email Anda" aria-label="Search">
+                    <button class="btn btn-contact" type="submit"><span>Subscribe</span></button>
+                </form>
+                <div class="social">
+                    <p style="color: white; font-size:19px; font-weight:400; margin-top: 8px; ">IKUTI KAMI</p>
+                    <div class="logo-social">
+                        <img src="/assets/img/icons.png" alt="All" class="img-social">
+                    </div>
+                </div>
+                <div class="call">
+                    <p>+62-88221038852</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
