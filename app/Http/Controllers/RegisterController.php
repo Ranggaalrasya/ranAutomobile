a5e97c1de205850d052b59e3b70b0f4244dbeaf1
WilliAndHan <william.a.hanantha@gmail.com>
2023-01-11 12:17:55 +0700

    Feat: [WA] add database user

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Foundation\Auth\User;
+
+class RegisterController extends Controller
+{
+    public function index(){
+        return view('register.index',["data_register" => User::all()]);
+    }
+
+    public function create(User $user)
+    {
+        return view('register.create', ["user" => User::all()
+    ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'name'      => 'required',
+            'email'     => 'required',
+            'password'  => 'required'
+        ]);
+
+        User::create($validateData);
+    }
+}
