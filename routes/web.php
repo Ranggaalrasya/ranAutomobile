ec64ff163e27c9c579fe23a02e37fd27891a6186
Vincencio Gabriel Frederic Sanjaya <vincenciogfs15@gmail.com>
2023-01-17 19:47:28 +0700

    Feat: [VG] Home UI Merk Mobil

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('merk');
 });
 
 Route::group(["prefix" => "/home"], function () {
