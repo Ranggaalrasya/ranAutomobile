8df4f672fe50c7c955491a610bc98fa2c827f8ef
Vincencio Gabriel Frederic Sanjaya <vincenciogfs15@gmail.com>
2023-01-18 11:40:48 +0700

    Fix: [VG] Fixing some margin error and combining

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="{{ asset('css/home/hero.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+        <link rel="stylesheet" href="css/home/merk.css">
         <title>Document</title>
     </head>
 
@@ -27,6 +27,93 @@
             </div>
         </div>
         <div class="form-check form-switch">
+        </div>
+
+        <div class="merk">
+            <h2 class="judul">Pilih Merk</h2>
+            <p class="desc">Pilih salah satu merek berikut ini untuk mendapat info mobil yang lebih lengkap</p>
+            <div class="container-merk-mobil">
+                <div class="container-merk-row">
+                    <div class="merk-mobil">
+                        <img src="assets/img/HYUNDAI.png" alt="">
+                        <p>HYUNDAI</p>
+                    </div>
+                    <div class="merk-mobil">
+                        <img src="assets/img/JEEP.png" alt="">
+                        <p>JEEP</p>
+                    </div>
+                    <div class="merk-mobil">
+                        <img src="assets/img/DAIHATSU.png" alt="">
+                        <p>DAIHATSU</p>
+                    </div>
+                    <div class="merk-mobil">
+                        <img src="assets/img/AUDI.png" alt="">
+                        <p>AUDI</p>
+                    </div>
+                    <div class="merk-mobil">
+                        <img src="assets/img/MITSUBISHI.png" alt="">
+                        <p>MITSUBISHI</p>
+                    </div>
+                    <div class="merk-mobil">
+                        <img src="assets/img/SUZUKI.png" alt="">
+                        <p>SUZUKI</p>
+                    </div>
+                </div>
+                <div class="container-merk-row" style="margin-right: 20px">
+                    <div class="merk-mobil">
+                        <img src="assets/img/VW.png" alt="">
+                        <p>VW</p>
+                    </div>
+                    <div class="merk-mobil">
+                        <img src="assets/img/NISSAN.png" alt="">
+                        <p>NISSAN</p>
+                    </div>
+                    <div class="merk-mobil">
+                        <img src="assets/img/MG.png" alt="">
+                        <p>MG</p>
+                    </div>
+                    <div class="merk-mobil">
+                        <img src="assets/img/MAZDA.png" alt="">
+                        <p>MAZDA</p>
+                    </div>
+                    <div class="merk-mobil">
+                        <img src="assets/img/RENAULT.png" alt="">
+                        <p>RENAULT</p>
+                    </div>
+                    <div class="merk-mobil">
+                        <img src="assets/img/MERCY.png" alt="">
+                        <p>MERCEDEZ</p>
+                    </div>
+                </div>
+                <div class="container-merk-row">
+                    <div class="merk-mobil">
+                        <img src="assets/img/BMW.png" width="128px" alt="">
+                        <p>BMW</p>
+                    </div>
+                    <div class="merk-mobil">
+                        <img src="assets/img/FUSO.png" alt="">
+                        <p>FUSO</p>
+                    </div>
+                    <div class="merk-mobil">
+                        <img src="assets/img/CHEVY.png" alt="">
+                        <p>CHEVROLET</p>
+                    </div>
+                    <div class="merk-mobil">
+                        <img src="assets/img/KIA.png" alt="">
+                        <p>KIA</p>
+                    </div>
+                    <div class="merk-mobil">
+                        <img src="assets/img/LEXUS.png" alt="">
+                        <p>LEXUS</p>
+                    </div>
+                    <div class="merk-mobil">
+                        <img src="assets/img/MINI.png" alt="">
+                        <p>MINI COOPER</p>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     </body>
 
