269bf7903a6b271b9f10971497101a29c00c5a5f
WilliAndHan <william.a.hanantha@gmail.com>
2023-01-18 11:48:18 +0700

    Add: [WA] Import controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
