da57ecb3fc527d967ffdd052adadf0ea180c9ed2
Vincencio Gabriel Frederic Sanjaya <vincenciogfs15@gmail.com>
2023-02-21 08:42:35 +0700

    Feat: [VG] Login & Register UI

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,29 +1,41 @@
 <nav class="navbar navbar-expand-lg" style="background-color:white;">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <img src="..\assets\img\black_logo.png" height="83" alt="">
-    </a>
-    <div class="collapse navbar-collapse" style="margin-left: 50px;" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item" >
-          <a class="nav-link active" style="font-size:19px; font-family: Montserrat; font-weight: 600;" aria-current="page" href="#">Mobil Baru</a>
-        </li>
-        <li class="nav-item" style="margin-left:30px;">
-          <a class="nav-link active" style="font-size:19px; font-family: Montserrat; font-weight: 600;" aria-current="page" href="#">Mobil Second</a>
-        </li>
-        <li class="nav-item" style="margin-left:30px;">
-          <a class="nav-link active" style="font-size:19px; font-family: Montserrat; font-weight: 600;" aria-current="page" href="#">Contact</a>
-        </li>
-      </ul>
-        <button class="btn" style="background: #FFFFFF;
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">
+            <img src="..\assets\img\black_logo.png" height="83" alt="">
+        </a>
+        <div class="collapse navbar-collapse" style="margin-left: 50px;" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" style="font-size:19px; font-family: Montserrat; font-weight: 600;"
+                        aria-current="page" href="#">Mobil Baru</a>
+                </li>
+                <li class="nav-item" style="margin-left:30px;">
+                    <a class="nav-link active" style="font-size:19px; font-family: Montserrat; font-weight: 600;"
+                        aria-current="page" href="#">Mobil Second</a>
+                </li>
+                <li class="nav-item" style="margin-left:30px;">
+                    <a class="nav-link active" style="font-size:19px; font-family: Montserrat; font-weight: 600;"
+                        aria-current="page" href="#">Contact</a>
+                </li>
+            </ul>
+            <button class="btn"
+                style="background: #FFFFFF;
         border: 1px solid #20BFB6;
-        border-radius: 4px; margin-right: 20px; height:35px; width:98px;" type="submit">
-          <p style="font-size:16px; font-family:Montserrat; font-weight: 600; color: #20BFB6;">Masuk</p>
-        </button>
-        <button class="btn" style="background: #20BFB6;
-        border-radius: 4px; margin-right:20px; height:35px; width:98px;" type="submit">
-          <p style="font-size:16px; font-family:Montserrat; font-weight: 600;">Daftar</p>
-        </button>
+        border-radius: 4px; margin-right: 20px; height:35px; width:98px;"
+                type="submit">
+                <a href="/login/index" style="text-decoration: none; color: #20BFB6">
+                    <p style="font-size:16px; font-family:Montserrat; font-weight: 600; color: #20BFB6;">Masuk</p>
+                </a>
+            </button>
+            <button class="btn"
+                style="background: #20BFB6;
+        border-radius: 4px; margin-right:20px; height:35px; width:98px;"
+                type="submit">
+                <a href="/register/index" style="text-decoration: none; color: #FFF">
+                    <p style="font-size:16px; font-family:Montserrat; font-weight: 600;">Daftar</p>
+                </a>
+
+            </button>
+        </div>
     </div>
-  </div>
 </nav>
