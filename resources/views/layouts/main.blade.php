991aee30868ad8046c075e20b5bf337f8b02df34
Vincencio Gabriel Frederic Sanjaya <vincenciogfs15@gmail.com>
2023-02-21 10:12:33 +0700

    Feat: [VG] Adding card in home

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,11 +10,18 @@
     <link rel="stylesheet" href="css/home/hero.css">
     <link rel="stylesheet" href="css/home/merk.css">
     <link rel="stylesheet" href="css/home/review.css">
+    <link rel="stylesheet" href="css/home/card.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    <style>
+        * {
+            font-family: 'Montserrat', sans-serif;
+        }
+    </style>
 </head>
 
 <body>
